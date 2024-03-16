fd837bd98fb18af76365d0f1386c3caaee6ffd16
Pranay Patle <pranayhpatle@gmail.com>
2024-03-16 11:36:59 +0530

    body parser added to webhook

--- src/router/clerkWebHook.ts
+++ src/router/clerkWebHook.ts
@@ -1,6 +1,7 @@
 import { clerkWebHook } from '../controllers/webhook';
 import express from 'express'
+import bodyParser from 'body-parser';
 export default (router: express.Router) => {
-  router.post("/webhook",  clerkWebHook);
+  router.post("/webhook",  bodyParser.raw({type: 'application/json'}),clerkWebHook);
  
 };
